88007a19eedecc989e1febe0d09fae83fd6f8bda
walecki.kontakt@gmail.com <walecki.kontakt@gamil.com>
2023-04-15 15:33:10 +0200

    second part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.scss';
 import Header from './components/Header';
+import Weather from './components/Weather';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Weather />
     </div>
   );
 }
---
+++ src/components/Weather.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+import Form from "react-bootstrap/Form";
+import axios from "axios";
+import "./Weather.scss";
+const CITIES = ["London", "München", "Wrocław"];
+
+const fetch = async (location) => {
+  const appid = `38ade7fb7fbfd60e57aed4f5a7a6cb56`;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${appid}`;
+  const { data } = await axios.get(url);
+  return data;
+};
+
+function Weather() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState(undefined);
+
+  useEffect(() => {
+    if (location) {
+      fetch(location).then((result) => {
+        return setData(result);
+      });
+      const interval = setInterval(() => {
+        fetch(location).then((result) => setData(result));
+      }, 3000);
+      return () => clearInterval(interval);
+    }
+  }, [location]);
+  console.log(data);
+
+  return (
+    <div className="weather ">
+      <Form.Select
+        onChange={(event) => {
+          setLocation(event.target.value);
+        }}
+      >
+        <option> Select City </option>
+        {CITIES.map((city) => {
+          return <option key={city}>{city}</option>;
+        })}
+      </Form.Select>
+
+      <Table striped bordered hover variant="dark" size="sm">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>HUMIDITY</th>
+            <th>TEMP</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {data.list ? <td>{data.list[0].dt_txt}</td> : null}
+            {data.list ? <td>{data.list[0].main.humidity}</td> : null}
+            {data.list ? <td>{data.list[0].main.temp}</td> : null}
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
+}
+
+export default Weather;
